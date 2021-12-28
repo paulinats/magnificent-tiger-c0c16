f22f2a33650607741b51baa182215a3c0ed67463
paulinats <71715041+paulinats@users.noreply.github.com>
2021-12-28 21:58:59 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../sass/main.scss';
 
 // This default export is required in a new `pages/_app.js` file.
+//bababa
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
